8dffd293ad84c6e2eba4b6cd5d82e01c3b3aae5c
Gilang Aryo <gilang91668@gmail.com>
2026-05-06 20:42:21 +0700

    feat: enhance setting controller responses with success messages and structured data

--- src/usecase/setting/setting.controller.js
+++ src/usecase/setting/setting.controller.js
@@ -1,42 +1,61 @@
-import { settingService } from './setting.service.js'
+import { settingService } from "./setting.service.js";
 
 export const settingController = {
     async getAll(req, res, next) {
         try {
-            const settings = await settingService.getAllSettings()
-            res.json(settings)
+            const settings = await settingService.getAllSettings();
+            res.json({
+                success: true,
+                message: "Settings fetched successfully",
+                data: settings,
+            });
         } catch (err) {
-            next(err)
+            next(err);
         }
     },
 
     async getByKey(req, res, next) {
         try {
-            const { key } = req.params
-            const setting = await settingService.getSettingByKey(key)
-            res.json(setting)
+            const { key } = req.params;
+            const setting = await settingService.getSettingByKey(key);
+            res.json({
+                success: true,
+                message: `Setting '${key}' fetched successfully`,
+                data: setting,
+            });
         } catch (err) {
-            next(err)
+            next(err);
         }
     },
 
     async updateByKey(req, res, next) {
         try {
-            const { key } = req.params
-            const updated = await settingService.updateSettingByKey(key, req.body)
-            res.json(updated)
+            const { key } = req.params;
+            const updated = await settingService.updateSettingByKey(
+                key,
+                req.body,
+            );
+            res.json({
+                success: true,
+                message: `Setting '${key}' updated successfully`,
+                data: updated,
+            });
         } catch (err) {
-            next(err)
+            next(err);
         }
     },
 
     async delete(req, res, next) {
         try {
-            const { key } = req.params
-            const deleted = await settingService.deleteSetting(key)
-            res.json({ message: `Setting '${key}' deleted`, deleted })
+            const { key } = req.params;
+            const deleted = await settingService.deleteSetting(key);
+            res.json({
+                success: true,
+                message: `Setting '${key}' deleted successfully`,
+                data: deleted,
+            });
         } catch (err) {
-            next(err)
+            next(err);
         }
     },
-}
+};
